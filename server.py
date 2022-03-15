38802c218cd693e5346b2dd1aed16f80c3098673
SeanTimmons <smtimmon@buffalo.edu>
2022-03-15 15:49:33 -0400

    Edited server.py

--- server.py
+++ server.py
@@ -3,10 +3,18 @@ import sys
 import json
 from pymongo import MongoClient
 
+mongo_client = MongoClient("mongo")
+db = mongo_client["cse312_project"]
+user_collection = db["users"]
+user_id_collection = db["user_id"]
+
 
 class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        return
+        recievedData = self.request.recv(1024)
+
+        sys.stdout.flush()
+        sys.stderr.flush()
 
 
 if __name__ == '__main__':
