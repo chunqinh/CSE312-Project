01537724374c9b148379751356e569dc0f02e5ad
chunqinh <62861612+chunqinh@users.noreply.github.com>
2022-03-28 17:47:03 -0400

    switch branch

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socketserver
 import sys
 import json
+import os
 import mysql.connector
 
 config = {
@@ -15,6 +16,20 @@ class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         recievedData = self.request.recv(1024)
         connection = mysql.connector.connect(**config)
+
+        if "POST".encode() in recievedData and "/login".encode() in recievedData:
+            print("hello")
+
+            # file_size_html = os.path.getsize('cse312-html/login.html')
+            # file_html = open("cse312-html/login.html", "r")
+            # read_html = file_html.read()
+            # print(read_html)
+            # frontend = "HTTP/1.1 200 OK\r\nContent-Length: " + str(file_size_html) + "\r\nContent-Type: text/html;\r\n X-Content-Type-Options: nosniff\r\n charset=utf-8\r\n\r\n" + read_html
+            # self.request.sendall(frontend.encode())
+        #get the username and password from request
+            #check in the database if that is in it
+            #if not return a message with invaild password or else redirect to dashboard
+
 
         sys.stdout.flush()
         sys.stderr.flush()
